43b7018dbc0815bf2812d050f903c4af8c6afb50
nkyenok <nkyenok@gmail.com>
2011-06-27 15:45:07 +0000

    added reg to the urls

--- Bank/urls.py
+++ Bank/urls.py
@@ -9,10 +9,11 @@ urlpatterns = patterns('',
     #url(r'^$', 'Bank.views.home', name='home'),
     # url(r'^Bank/', include('Bank.foo.urls')),
     url(r'^bank/',include('Banking-System.Bank.urls')),
+    url(r'^reg/', include('Bank.reg.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
