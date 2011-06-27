4943dad2541635dc5d7555210f0084adf80bf1c8
Kaf <a.kafu@yahoo.com>
2011-06-27 15:53:39 +0000

    made changes to urls.py

--- Bank/urls.py
+++ Bank/urls.py
@@ -8,11 +8,11 @@ urlpatterns = patterns('',
     # Examples:
     #url(r'^$', 'Bank.views.home', name='home'),
     # url(r'^Bank/', include('Bank.foo.urls')),
-    url(r'^bank/',include('Banking-System.Bank.urls')),
+    url(r'^bank/',include('Bank.bank.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
