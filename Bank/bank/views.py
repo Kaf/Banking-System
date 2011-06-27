a88272c2eb4d6ea42631075d9f8d72e45b08f9de
Kaf <a.kafu@yahoo.com>
2011-06-27 11:43:54 +0000

    Updated views.py

--- Bank/bank/views.py
+++ Bank/bank/views.py
@@ -22,3 +22,17 @@ def create_account(request):
     		return HttpResponse(t.render(c))
 
 
+# Create your views here.
+
+class transferForm(forms.Form):
+	From = forms.PositiveIntegerField()
+	To = forms.PositiveIntegerField()
+	Amount = forms.FloatField()
+	def __unicode__(self):
+		return self.From
+
+def transfer(request):
+	transfer = transferForm.objects.all()
+	t = loader.get_template('bank/transfer.html')
+	c = Context({'transfer':transfer})
+	return HttpResponse(t.render(c))
