008c914d8ccbacff72a41f27f0791ce858417900
Kaf <a.kafu@yahoo.com>
2011-06-27 12:44:42 +0000

    Added views.py

--- Bank/bank/views.py
+++ Bank/bank/views.py
@@ -35,8 +35,8 @@ def transfer(request):
 	transfer = transferForm.objects.all()
 	for Person.accnum == transfer.From:
 		Person.amount= Person.amount -transfer.amount
-		for Person.accnum == transfer.To:
-			Person.amount = Person.amount + transer.amount
+	for Person.accnum == transfer.To:
+		Person.amount = Person.amount + transer.amount
 	print 'Transfer Successful'
 	t = loader.get_template('bank/transfer.html')
 	c = Context({'transfer':transfer})
