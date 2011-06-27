bd907c1b8e38d8be83c0d5005f72d942d8c0fabb
Kaf <a.kafu@yahoo.com>
2011-06-27 13:57:57 +0000

    edited urls.py

--- Bank/urls.py
+++ Bank/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Bank.views.home', name='home'),
+    #url(r'^$', 'Bank.views.home', name='home'),
     # url(r'^Bank/', include('Bank.foo.urls')),
+    url(r'^bank/',include('Banking-System.Bank.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
