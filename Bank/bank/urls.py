026fdbca69b0318787de7750666e6df6c11c4b06
Kaf <a.kafu@yahoo.com>
2011-06-27 15:48:00 +0000

    update

--- Bank/bank/urls.py
+++ Bank/bank/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
 #url(r'^$', 'bank.view.home'),
 url(r'^bank/transfer/$', 'bank.views.transfer'),
-url(r'^bank/view/$', 'bank.views.viewMoney')
+url(r'^bank/view/$', 'bank.views.viewMoney'),
 url(r'^bank/create/$', 'bank.views.create_account'),
-url(r'^bank/', 'bank.views.homepage'),)
+url(r'^bank/$', 'bank.views.homepage'),)
 
