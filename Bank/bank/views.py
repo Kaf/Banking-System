79397fb986ff668d19dd7f7232530b40845f9121
Kaf <a.kafu@yahoo.com>
2011-06-27 11:29:10 +0000

    added a transfer form

--- Bank/bank/views.py
+++ Bank/bank/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+
+class transferForm(forms.Form):
+	From = forms.PositiveIntegerField()
+	To = forms.PositiveIntegerField()
+	Amount = forms.FloatField()
+	def __unicode__(self):
+		return self.From
+
+def transfer(request):
+	transfer = transferForm.objects.all()
+	t = loader.get_template('bank/transfer.html')
+	c = Context({'transfer':transfer})
+	return HttpResponse(t.render(c))
